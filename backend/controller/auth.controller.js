7bd7bcec1612296278f5bdba94d2501a653b3106
Abinrd <abinrajuvilayil@gmail.com>
2025-02-15 14:28:09 +0530

    Signin controller

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -58,6 +58,36 @@ export const signUp = async (req, res, next) => {
    
 
 export const signIn=async(req,res,next)=>{
+    try{
+        const {email,password}=req.body
+        const user=await User.findOne({email})
+        if(!user){
+            const error=new Error('Invalid User');
+            error.statusCode=404;
+            throw error;
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid){
+            const error=new Error('Invalid Password');
+            error.statusCode=401;
+            throw error;
+        }
+        const token= jwt.sign({userId:user._id},JWT_SECRET,{expiresIn:JWT_EXPIRES_IN});
+
+        return res.status(200).json({
+            success:true,
+            message:'User Signed-in succesfully',
+            data:{
+                token,
+                user,
+            }
+
+        })
+
+    }catch(error){
+        next(error);
+    }
     
 }
 
