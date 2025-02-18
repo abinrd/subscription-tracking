55f8fd2f0a4815761cea2094c173421d95ddc98c
Abinrd <abinrajuvilayil@gmail.com>
2025-02-18 22:27:04 +0530

    getUserSubscriptions controller

--- backend/routes/subscription.route.js
+++ backend/routes/subscription.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import authorize from "../middleware/auth.middleware.js";
-import { createSubscription } from "../controller/subscription.controller.js";
+import { createSubscription, getUserSubscriptions } from "../controller/subscription.controller.js";
 const subscriptionRouter=Router();
 
-subscriptionRouter.get('/',(req,res)=>{
+subscriptionRouter.get('/',authorize,(req,res)=>{
     res.send({title:"GET all subscription details"})
 })
 
@@ -13,7 +13,7 @@ subscriptionRouter.get('/:id',(req,res)=>{
 })
 
 
-subscriptionRouter.post('/',authorize,createSubscription);
+subscriptionRouter.post('/',authorize,createSubscription); //Create a subscription
 
 
 subscriptionRouter.put('/:id',(req,res)=>{
@@ -25,9 +25,7 @@ subscriptionRouter.delete('/:id',(req,res)=>{
     res.send({title:"DELETE a subscription details"})
 })
 
-subscriptionRouter.get('/user/:id',(req,res)=>{
-    res.send({title:"GET all user subscription details"})
-})
+subscriptionRouter.get('/user/:id',authorize,getUserSubscriptions) //Get all subscriptions linked with the user
 
 subscriptionRouter.put('/:id/cancel',(req,res)=>{
     res.send({title:"CANCEL a user subscription details"})
