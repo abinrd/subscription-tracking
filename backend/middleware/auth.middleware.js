150243e4dfa26c59d17d1b87dcaa5d75c91502dd
Abinrd <abinrajuvilayil@gmail.com>
2025-02-24 21:03:00 +0530

    revert

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,37 +1,29 @@
-import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
-import { JWT_SECRET } from '../config/env.js';
+import jwt from 'jsonwebtoken'
+import {  JWT_SECRET } from '../config/env.js';
 
-export const authorize = async (req, res, next) => {
-    try {
-        let token;
+const authorize = async(req,res,next)=>{
+    try{
+        let token ;
 
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(" ")[1];
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(" ")[1]
         }
-
-        if (!token) {
-            return res.status(401).json({ message: 'Unauthorized Access' });
+        if(!token){
+            return res.status(401).json({message:'Unauthorized Access'})
         }
+        const decoded= jwt.verify(token,JWT_SECRET)
+        const user = await User.findById(decoded.userId);
 
-        const decoded = jwt.verify(token, JWT_SECRET);
-        const user = await User.findById(decoded.userId).select('-password');
-
-        if (!user) {
-            return res.status(401).json({ message: 'Unauthorized Access' });
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token',error });
+        if(!user){
+            res.status(401).json({message:'Unauthorized Access'})
     }
-};
+    req.user=user;
+    next();
+    
+    
+}catch(error){
+    res.status(401).json({message:'Unauthorized Access',error:error.message})
+}}
 
-export const isAdmin = (req, res, next) => {
-    if (req.user && req.user.role === 'admin') {
-        next();
-    } else {
-        return res.status(403).json({ message: 'Access denied. Admins only' });
-    }
-};
+export default authorize
